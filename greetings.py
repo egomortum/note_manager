33167b8daebb8cff6b020a25cf2acae339f18a9c
egomortum <fermaedy154@gmail.com>
2024-12-25 01:10:36 +0400

    Add files via upload

---
+++ greetings.py
@@ -0,0 +1,13 @@
+username = 'Лилия'
+title = 'Поздравление'
+content = 'поздравить родственника'
+status = 'активна'
+create_date = '23-06-2023'
+issue_date = '02-07-2023'
+
+print('имя пользователя', username)
+print('заголовок', title)
+print('описание', content)
+print('статус', status)
+print('дата создания', create_date)
+print('дата истечения', issue_date)
