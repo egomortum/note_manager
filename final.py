e17e5461bc2c42d7b07bf8739ac82b16d91a489a
egomortum <fermaedy154@gmail.com>
2024-12-29 03:14:30 +0400

    Add files via upload

---
+++ final.py
@@ -0,0 +1,20 @@
+username = input('Введите своё имя: ')
+title1 = input('Введите заголовок заметки: ')
+title2 = input('Введите заголовок заметки: ')
+title3 = input('Введите заголовок заметки: ')
+titles = [title1, title2, title3]
+content = input('Введите содержание заметки: ')
+status = input('Введите статус заметки ')
+create_date = input('Введите дату создания заметки в формате: дд.мм.гг ')
+issue_date = input('Введите дату истечения действия заметки в формате: дд.мм.гг ')
+
+note = [
+    username,
+    titles,
+    content,
+    status,
+    create_date,
+    issue_date
+]
+
+print(note)
